9a3e29ea19c3bf42ab337d21ab3a020161328bbd
Pavel Horal <pavel.horal@orchitech.cz>
2017-11-08 22:07:32 +0100

    STRINGS diff command.

--- script/strings.js
+++ script/strings.js
@@ -1,28 +1,61 @@
 #!/usr/bin/env node
 'use strict';
-/**
- * Search for text in STRINGS.
- */
 var parseStrings = require('./parse/parse-strings'),
     renderStringId = require('./utils/render-formId'),
     program = require('commander'),
     fs = require('fs');
 
 program.
-    usage('[options] <pattern>').
+    option('-o --output <file>', 'Export STRINGS to a file.');
+
+var output = program.output ? fs.createWriteStream(program.output, 'wx') : process.stdout;
+
+function readStrings(filePath) {
+    return new parseStrings.StringsReader().readFile(filePath);
+}
+
+program.
+    command('find <pattern>').
+    description('Search for text in STRINGS.').
     option('-s, --strings <file>', 'Path to a STRINGS file.').
     option('-f, --flags <flags>', 'Additional regexp flags.').
-    parse(process.argv);
+    action((pattern, options) => {
+        var strings = readStrings(options.strings),
+            regexp = new RegExp(pattern, options.flags)
+        Object.keys(strings).forEach(stringId => {
+            var renderedId = renderStringId(stringId);
+            if (!regexp.test(renderedId) && !regexp.test(strings[stringId])) {
+                return; // Not a match
+            }
+            output.write(`[MATCH] ${renderedId} ${JSON.stringify(strings[stringId])}\n`);
+        });
+    });
 
+program.
+    command('diff <first> <second>').
+    description('Compare two STRINGS files and find differences.').
+    action((first, second) => {
+        var strings = [readStrings(first), readStrings(second)],
+            currentId = null;
+        Object.keys(strings[0]).concat(Object.keys(strings[1])).sort().forEach(stringId => {
+            if (stringId === currentId) {
+                return;
+            }
+            currentId = stringId;
+            if (strings[0][currentId] === strings[1][currentId]) {
+                return;
+            } else if (strings[0][currentId]) {
+                output.write(`- ${renderStringId(currentId)} ${JSON.stringify(strings[0][currentId])}\n`);
+            } else if (strings[1][currentId]) {
+                output.write(`+ ${renderStringId(currentId)} ${JSON.stringify(strings[1][currentId])}\n`);
+            }
+        });
+    });
+
+program.parse(process.argv);
+if (program.output) {
+    output.end();
+}
 if (!program.args.length) {
     program.help();
 }
-
-var strings = new parseStrings.StringsReader().readFile(program.strings),
-    pattern = new RegExp(program.args[0], program.flags);
-
-Object.keys(strings).forEach(stringId => {
-    if (pattern.test(strings[stringId])) {
-        console.log('[MATCH]', renderStringId(stringId), strings[stringId]);
-    }
-});
